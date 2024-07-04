1a6e4e0d0e738f7c2caecbb092a1ecdd37aa1c3c
RuanDevJs <ruanelpidio@hotmail.com>
2024-07-04 12:21:49 -0300

    Updated Project Skateboard

--- App.tsx
+++ App.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
-import Home from './src/Screens/Home';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import Home from "./src/Screens/Home";
 
 export default function App() {
-  return <Home />
+  return <Home />;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
